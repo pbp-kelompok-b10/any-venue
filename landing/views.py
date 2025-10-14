eef38e14973a467a4a8e431677618baa6de98cbf
kurrorro <keishavnia@gmail.com>
2025-10-15 00:26:43 +0700

    landing page lagi

--- landing/views.py
+++ landing/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def show_main(request):
-    return render(request, "hello_world.html")
+    return render(request, "landing.html")
 
 def features_preview(request):
     return render(request, "sections/features_preview.html")
