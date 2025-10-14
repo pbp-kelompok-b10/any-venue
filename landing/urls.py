6fce91dd7037baa089fe174bc7dc468234e6a69e
Alyanabilla <alyanabila486@gmail.com>
2025-10-15 01:20:58 +0700

    Add booking preview section and update views/urls

--- landing/urls.py
+++ landing/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from landing.views import (
-    show_main, features_preview, header_test,
+    show_main, features_preview, header_test, booking_preview_test
 )
 
 app_name = 'landing'
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('features/', features_preview, name='features_preview'),
     path('header/', header_test, name='header_test'),
+    path('booking-preview/', booking_preview_test, name='booking_preview_test'),
 ]
