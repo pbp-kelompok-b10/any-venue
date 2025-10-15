7c468649e88060c486e3a3f09cd3684fd4d47267
kurrorro <keishavnia@gmail.com>
2025-10-15 09:40:29 +0700

    navbar

--- landing/urls.py
+++ landing/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from landing.views import (
-    show_main, features_preview, header_test, booking_preview_test
+    show_main, features_preview, header_test, features_review, booking_preview_test
 )
 
 app_name = 'landing'
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('features/', features_preview, name='features_preview'),
     path('header/', header_test, name='header_test'),
+    path('review/', features_review, name='features_review'),
     path('booking-preview/', booking_preview_test, name='booking_preview_test'),
 ]
