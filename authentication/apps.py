028555ff5bc2cf746985991529c80d4be0e2ebf5
Alyanabilla <alyanabila486@gmail.com>
2025-10-22 16:19:34 +0700

    add user profile model and signals for automatic profile creation on user registration

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
-
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+
+class UserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'user'
+
+    def ready(self):
+        import user.signals
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,20 @@
+# auth/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ('USER', 'User'),
+        ('OWNER', 'Owner'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='USER')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def is_owner(self):
+        return self.role == self.Role.OWNER
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
