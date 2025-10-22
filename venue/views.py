c5f64ec1e5b4ed19acc5e3979f40af72eddf4887
kurrorro <keishavnia@gmail.com>
2025-10-22 19:37:30 +0700

    add main for venue card & views

--- venue/views.py
+++ venue/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import get_object_or_404, render
-
-from venue.models import Venue
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Venue, City, Category
+from user.models import Profile
 
 # Create your views here.
+@login_required(login_url='/login')
+def show_main(request): 
+    filter_type = request.GET.get("filter", "all")  # default 'all'
+
+    if filter_type == "all":
+        products = Venue.objects.all()
+    else:
+        products = Venue.objects.filter(user=request.user)
+        
+    context = {
+        'last_login': request.COOKIES.get('last_login', 'Never'),
+        'user': request.user,
+    }
+
+    return render(request, "main.html", context)
+
 def show_details(request):
     venue = get_object_or_404(Venue, id=65)
     context = {
