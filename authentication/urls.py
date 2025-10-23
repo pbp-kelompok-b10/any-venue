fba16db948e9f42618b924fc3c94963e7611a966
Alyanabilla <alyanabila486@gmail.com>
2025-10-24 01:21:08 +0700

    Fix formatting in URL configurations by adding missing commas

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from . import views
 from authentication.views import register_view, login_view, logout_view
 
 app_name = 'authentication'
+
 urlpatterns = [
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
