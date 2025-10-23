9fa80edf499c914c18af7019e08b08a37b17751d
Alyanabilla <alyanabila486@gmail.com>
2025-10-23 21:36:52 +0700

    Update migration timestamps, refine profile page logic, and enhance template structure

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from account.views import profile_page
+from .views import profile_page
 
 urlpatterns = [
     path("", profile_page, name="profile_page"),
-    # path("delete-booking/", views.delete_booking, name="delete_booking"),
+    
 ]
--- account/views.py
+++ account/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
-from .models import Profile#, Booking
-
+from account.models import Profile
+from booking.models import Booking
+from review.models import Review
+from venue.models import Venue
 
 def profile_page(request):
     if request.user.is_authenticated:
         profile = get_object_or_404(Profile, user=request.user)
-        bookings = profile.bookings.all().order_by('-date')
+        if profile.is_owner:
+            venues = profile.venues.all()
+            return render(request, "profile_page.html", {
+                "profile": profile,
+                "venues": venues
+            })
+        else:  # biasa user
+            bookings = request.user.bookings.all()
+            reviews = Review.objects.filter(user=profile)
+            return render(request, "profile_page.html", {
+                "profile": profile,
+                "bookings": bookings,
+                "reviews": reviews
+            })
     else:
+        # user belum login: coba ambil username terakhir login, atau profile kosong
         profile = None
-        bookings = None
-    return render(request, "profile_page.html", {"profile": profile, "bookings": bookings})
-
-
-# def profile_page(request):
-#     profile = get_object_or_404(Profile, user__username=request.user.username if request.user.is_authenticated else None)
-#     # bookings = profile.bookings.all().order_by('-date') if request.user.is_authenticated else None
-#     return render(request, "profile_page.html", {"profile": profile}) # "bookings": bookings
-
-# @login_required
-# def delete_booking(request):
-#     if request.method == "POST":
-#         booking_id = request.POST.get("id")
-#         booking = get_object_or_404(Booking, id=booking_id, profile__user=request.user)
-#         booking.delete()
-#         return JsonResponse({"status": "ok"})
-#     return JsonResponse({"status": "error"}, status=400)
+        return render(request, "profile_page.html", {"profile": profile})
