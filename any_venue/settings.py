463bfdc1445934f3ea724c1c7c3679b9a8387272
FakhriHusainiRomza23 <fakhri.husaini41@ui.ac.id>
2025-10-24 19:06:34 +0700

    Menyelesaikan styling dan menambahkan kalender

--- any_venue/settings.py
+++ any_venue/settings.py
@@ -138,7 +138,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# Use Jakarta time so past slots today are calculated correctly for Indonesia users
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
