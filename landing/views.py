364f7174f1f3c988853b12f8dd2dad202e82ed86
kurrorro <keishavnia@gmail.com>
2025-10-24 22:39:30 +0700

    fix landing

--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from review.models import Review
+from venue.models import Venue
+
 # Create your views here.
 def show_landing(request):
-    return render(request, "landing.html")
+    latest_venues = Venue.objects.order_by('-pk')[:3]
+    latest_reviews = Review.objects.order_by('-created_at')[:3]
+    context = {
+        'venues_list': latest_venues,
+        'reviews_list': latest_reviews,
+    }
+
+    return render(request, "landing.html", context)
 
 def features_preview(request):
     return render(request, "sections/features_preview.html")
